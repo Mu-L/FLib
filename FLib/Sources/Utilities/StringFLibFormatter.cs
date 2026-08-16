4d607113f5baedc26d958527118a6a49a710bdc1
yqb-book <@>
2026-08-16 18:46:49 +0800

    refactor(formatter): 优化字符串格式化实现逻辑
    - 使用switch表达式简化格式判断和处理逻辑
    - 支持‰和‱两种新格式表示千分号和万分号
    - 统一使用Convert.ToDouble替代Convert.ToSingle保证精度一致
    - 删除未使用的using引用，简化代码结构
    - 保持对abs格式的支持并优化返回逻辑
    - 改进默认格式化的调用方式，提升代码可读性和维护性

--- FLib/Sources/Utilities/StringFLibFormatter.cs
+++ FLib/Sources/Utilities/StringFLibFormatter.cs
@@ -1,9 +1,7 @@
 ﻿//==================={By Qcbf|qcbf@qq.com|12/10/2022 4:30:36 PM}===================
 
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using FLib;
 
 namespace FLib
 {
@@ -13,27 +11,19 @@ namespace FLib
 
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
-            if (format?.StartsWith('%') == true) // 百分比, 每多一个% 就会乘以0.1
+            if (string.IsNullOrEmpty(format))
+                return string.Format(formatProvider, format!, arg);
+            return format[0] switch
             {
-                var val = 1f;
-                for (var i = 1; i < format.Length; i++)
-                {
-                    if (format[i] == '%') val *= 0.1f;
-                    else break;
-                }
-                return (val * Convert.ToSingle(arg)).ToString("0.##") + "%";
-            }
-            if (format?.StartsWith('+') == true)
-                return (Convert.ToSingle(arg) + format[1..].AsSpan().ToFloat()).ToString("0.##");
-            if (format?.StartsWith('-') == true)
-                return (Convert.ToSingle(arg) - format[1..].AsSpan().ToFloat()).ToString("0.##");
-            if (format?.StartsWith('*') == true)
-                return (Convert.ToSingle(arg) * format[1..].AsSpan().ToFloat()).ToString("0.##");
-            if (format?.StartsWith('/') == true)
-                return (Convert.ToSingle(arg) / format[1..].AsSpan().ToFloat()).ToString("0.##");
-            if (format == "abs")
-                return Math.Abs(Convert.ToDouble(arg)).ToString(CultureInfo.InvariantCulture);
-            return arg.ToString();
+                '%' => (Convert.ToDouble(arg) * 0.01).ToString("0.##") + "%",
+                '‰' => (Convert.ToDouble(arg) * 0.001).ToString("0.##") + "%",
+                '‱' => (Convert.ToDouble(arg) * 0.0001).ToString("0.##") + "%",
+                '+' => (Convert.ToDouble(arg) + format.AsSpan(1).ToDouble()).ToString("0.##"),
+                '-' => (Convert.ToDouble(arg) - format.AsSpan(1).ToDouble()).ToString("0.##"),
+                '*' => (Convert.ToDouble(arg) * format.AsSpan(1).ToDouble()).ToString("0.##"),
+                '/' => (Convert.ToDouble(arg) / format.AsSpan(1).ToDouble()).ToString("0.##"),
+                _ => format == "abs" ? Math.Abs(Convert.ToDouble(arg)).ToString(CultureInfo.InvariantCulture) : string.Format(formatProvider, format, arg)
+            };
         }
 
         public object GetFormat(Type formatType)
