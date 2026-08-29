f05f999c98c3542362a674246fdbe52555bd79e3
yqb <@>
2026-08-29 10:55:51 +0800

    refactor(time-logics): 重命名生命周期方法以统一命名规范
    - 将所有 TimeLogicClip 子类中的 Begin 方法改为 OnPlay，提高语义清晰度
    - 将所有 End 方法改为 OnStop，统一播放停止时的调用接口
    - 替换 Update 方法为 OnUpdate，增加命名一致性
    - 替换 LateUpdate 方法为 OnLateUpdate，保持风格统一
    - 修改 TimeLogicTrack 和 TimeLogic 中调用 Begin/End/Update/LateUpdate 的位置同步新方法名
    - 调整编辑器相关代码调用以匹配方法重命名，避免潜在错误
    - 清理部分无用字段及代码，简化逻辑流程，提高阅读性

--- FLib/Sources/WorldCores/TimeLogics/TimeLogicTrack.cs
+++ FLib/Sources/WorldCores/TimeLogics/TimeLogicTrack.cs
@@ -44,13 +44,13 @@ namespace FLib.WorldCores.TimeLogics
                 return;
             var temp = CurrentClip;
             CurrentClip = null;
-            temp.End();
+            temp.OnStop();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        public virtual void Update()
+        public virtual void OnUpdate()
         {
             var frame = Root.CurrentFrame;
             if (CurrentClip != null)
@@ -59,11 +59,11 @@ namespace FLib.WorldCores.TimeLogics
                 {
                     if (CurrentClip.CheckFrame(frame))
                     {
-                        CurrentClip.Update();
+                        CurrentClip.OnUpdate();
                         return;
                     }
 
-                    CurrentClip.End();
+                    CurrentClip.OnStop();
                     CurrentClip = null;
                 }
                 catch (Exception e)
@@ -80,8 +80,8 @@ namespace FLib.WorldCores.TimeLogics
                     try
                     {
                         CurrentClip = clip;
-                        CurrentClip.Begin();
-                        CurrentClip.Update();
+                        CurrentClip.OnPlay();
+                        CurrentClip.OnUpdate();
                     }
                     catch (Exception e)
                     {
@@ -94,8 +94,8 @@ namespace FLib.WorldCores.TimeLogics
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public virtual void LateUpdate()
-            => CurrentClip?.LateUpdate();
+        public virtual void OnLateUpdate()
+            => CurrentClip?.OnLateUpdate();
 
 #if UNITY_PROJ
         public T GetExternalReference<T>(in ExternalReferenceField<T> field) where T : class => field.Index < 0 ? null : Root.ExternalReferences[field.Index] as T;
