ce5d0dbe185943543bd3436d8446ac594a33e6f2
yqb-book <@>
2026-09-13 16:41:34 +0800

    refactor(world-effect): 优化注释中的无限符号表述
    - 将最大叠加层数的注释中的“0无限”修改为“0♾”
    - 将持续时间的注释中的“0无限”修改为“0♾”

--- FLib/Sources/WorldCores/Effects/WorldEffectBase.cs
+++ FLib/Sources/WorldCores/Effects/WorldEffectBase.cs
@@ -10,9 +10,9 @@ namespace FLib.WorldCores.Effects
     [BytesPackGenHoldKey(2)]
     public abstract unsafe class WorldEffectBase : IBytesPackable
     {
-        [Comment("最大叠加层数(0无限)")] public ushort MaxStackCount = 1;
+        [Comment("最大叠加层数(0♾)")] public ushort MaxStackCount = 1;
         [Comment("重复添加方式")] public EWorldEffectAddOption AddOption;
-        [Comment("持续时间")] public FNum Duration;
+        [Comment("持续时间(0♾)")] public FNum Duration;
 
         [NonSerialized] public uint Id;
         [NonSerialized] public ushort StackCount;
